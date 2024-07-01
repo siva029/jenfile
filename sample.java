0069a5df5ba273d7aaa8d0dd01be85caea985d48
siva029 <23203029@srcas.ac.in>
2024-07-01 12:23:01 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,8 +1,8 @@
-class sample(
+class sample{
   public static void main(String[]args)
   {
   System.out.println("Hello");
-  System.out.println("to");
+  System.out.println("To");
   System.out.println("Deveops");
   System.out.println("Class");
 }
