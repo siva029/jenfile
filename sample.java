439df3b70717b28fc4247dad62d0cdfe6fa431ef
siva029 <23203029@srcas.ac.in>
2024-07-01 11:58:41 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,13 @@
+class sample(
+  public static void main(String[]args)
+  {
+  System.out.println("Hello");
+  System.out.println("to");
+  System.out.println("Deveops");
+  System.out.println("Class");
+}
+}
+  
+  
+  
+  
